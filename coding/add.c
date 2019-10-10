4ddd744a31e6d35f184f28fb90bbb3d3f469efc3
gauravchauhan2403 <56407052+gauravchauhan2403@users.noreply.github.com>
2019-10-11 00:23:20 +0530

    Create add.c

---
+++ coding/add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{ int n,m,sum=0;
+   printf("Enter the number whose addition is needed/n");
+   scanf("%d %d",&n,&m);
+   sum=m+n;
+   printf("The sum is %d/n",sum);
+   return 0;
+   }
+   
